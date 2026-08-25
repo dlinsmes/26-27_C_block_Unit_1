45a9a4eee3eb92247de73d41a1aa5c8263772c76
davidlin <dlin@smes.org>
2026-08-25 13:44:30 -0700

    Day 1

---
+++ src/Intro.java
@@ -0,0 +1,28 @@
+// code comments start with //
+//code comments are used for notes and they
+//do not run as code
+
+//java classes are program files
+//class names need to start with a Capital letter
+//and have no spaces
+
+//the class name and the file name must match
+public class Intro {
+
+    //main method
+    //for now, all code we write needs to go in the main method
+    public static void main(String [] args) {
+
+        //output to the console - same as console.log in js
+        System.out.print("hi");
+        //"hi" in quotes is a string - data type that can
+        //contain letters, numbers, and characters
+
+        //if hi was NOT in quotes, java would think it's a variable
+        //but it will be red and won't run because a hi variable
+        //has not been declared
+//        System.out.print(hi);
+
+    }
+
+}
